4f8afc9153ff2892ef258f32053d24df0af1b68e
unknown <jhlee0637@gmail.com>
2025-01-23 18:41:04 +0900

    Leetcode 135

--- Leet Code/Top Interview 150/135.candy.py
+++ Leet Code/Top Interview 150/135.candy.py
@@ -7,16 +7,16 @@
 # @lc code=start
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = [1 for n in range(len(ratings))] #every students have at least one candy
-        for i in range(1, len(ratings)): #search from left to right
+        candies = [1 for n in range(len(ratings))] #every students have at least one candy #s: O(n)
+        for i in range(1, len(ratings)): #search from left to right #t: O(n)
             prevRate = ratings[i-1]
             rate = ratings[i]
             if prevRate<rate and candies[i-1]>=candies[i]: #if the present rate is higher but the candy is lower
                 candies[i]=candies[i-1]+1 #replace the candy to the amount one candy more than the previous rate
 
-        ratings=ratings[::-1] #to search from right to left, change the array direction
-        candies=candies[::-1]
-        for i in range(1, len(ratings)): #repeat
+        ratings=ratings[::-1] #to search from right to left, change the array direction #t: O(n) s:O(n)
+        candies=candies[::-1] #t: O(n) s:O(n)
+        for i in range(1, len(ratings)): #repeat #t: O(n)
             prevRate = ratings[i-1]
             rate = ratings[i]
             if prevRate<rate and candies[i-1]>=candies[i]:
@@ -25,6 +25,8 @@ class Solution:
         answer=sum(candies)
         return answer
     
+#Time complexity: O(n)
+#Space complexity: O(n)
 
 # @lc code=end
 
